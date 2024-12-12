d9a9320caf4b589ce3962a0443cc9a17973a4173
Kacey Halstead <114954301+Kacey-Halstead@users.noreply.github.com>
2024-12-12 17:54:01 +0000

    Added Dear ImGui and ImPlot

--- FYP_AIBelievability/FYP_AIBelievability/FYP_AIBelievability.cpp
+++ FYP_AIBelievability/FYP_AIBelievability/FYP_AIBelievability.cpp
@@ -9,77 +9,166 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
+#include <imgui.h>
+#include <implot.h>
+#include <imgui_impl_sdl2.h>
+#include <imgui_impl_sdlrenderer2.h>
+
 #include "WFC.h"
 
-struct SDLVars
+struct InitVars
 {
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
+	bool initFail = false;
 };
 
-SDLVars* InitSDL()
+int   bar_data[2] = { 1, 2};
+float x_data[11] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+float y_data[11] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+InitVars InitSDL()
 {
-	SDLVars* sdlVars{};
+	InitVars initVars{};
 
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0) //initialise SDL
 	{
 		std::cout << "Unable to initialise SDL: " << SDL_GetError();
-		return nullptr;
+		initVars.initFail = true;
 	}
 
 	if (IMG_Init(IMG_INIT_PNG) == 0) {
 		std::cout << "Error SDL2_image Initialization";
-		return nullptr;
+		initVars.initFail = true;
 	}
 
 	//Create window
-	sdlVars->window = SDL_CreateWindow("WFC", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL); //name, horizontal pos, vertical pos, width, height, mode
-	if (sdlVars->window == NULL) {
+	initVars.window = SDL_CreateWindow("WFC", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL); //name, horizontal pos, vertical pos, width, height, mode
+	if (initVars.window == NULL) {
 		std::cout << "Error window creation";
-		return nullptr;
+		initVars.initFail = true;
 	}
 
 	//Create renderer
-	sdlVars->renderer = SDL_CreateRenderer(sdlVars->window, -1, SDL_RENDERER_ACCELERATED); //window to render to, index of rendering driver (-1 = first available), mode
-	if (sdlVars->renderer == NULL) {
+	initVars.renderer = SDL_CreateRenderer(initVars.window, -1, SDL_RENDERER_ACCELERATED); //window to render to, index of rendering driver (-1 = first available), mode
+	if (initVars.renderer == NULL) {
 		std::cout << "Error renderer creation";
-		return nullptr;
+		initVars.initFail = true;
 	}
-}
 
-int main(int argc, char* argv[])
-{
-	SDLVars* sdlVars = InitSDL();
-	if (sdlVars == nullptr) return 0; //if init error
+	//IMGUI
+	ImGui::CreateContext();
+	ImPlot::CreateContext();
+	ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplSDL2_InitForSDLRenderer(initVars.window, initVars.renderer);
+	ImGui_ImplSDLRenderer2_Init(initVars.renderer);
 
 	srand(time(NULL));
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
+	return initVars;
+}
+
+int main(int argc, char* argv[])
+{
+	InitVars returnVal = InitSDL();
+	if (returnVal.initFail == true) return 0; //if init error
+	InitVars* initVars = &returnVal;
+
 	//WFC Init
-	WFC* WFCComponent = new WFC(10, 10, sdlVars->window, sdlVars->renderer);
+	WFC* WFCComponent = new WFC(10, 10, initVars->window, initVars->renderer);
 	WFCComponent->WFCBody();
 	WFCComponent->CreateRects();
+
+	bool show_demo_window = true;
+	bool show_another_window = false;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 	//main loop
 	while (true)
 	{
 		SDL_Event e;
-		if (SDL_PollEvent(&e)) //ends loop if exit button pressed
+		if (SDL_PollEvent(&e))
 		{
+			ImGui_ImplSDL2_ProcessEvent(&e);
+
 			if (e.type == SDL_QUIT)
+			{
+				break;
+			}
+			if (e.type == SDL_WINDOWEVENT && e.window.event == SDL_WINDOWEVENT_CLOSE && e.window.windowID == SDL_GetWindowID(initVars->window))
 			{
 				break;
 			}
 		}
 
-		SDL_RenderClear(sdlVars->renderer); //remove anything already rendered
-		WFCComponent->RenderWFC(sdlVars->renderer); //render WFC 
-		SDL_RenderPresent(sdlVars->renderer); //moving from back buffer to screen
+		// Start the Dear ImGui frame
+		ImGui_ImplSDLRenderer2_NewFrame();
+		ImGui_ImplSDL2_NewFrame();
+		ImGui::NewFrame();
+
+
+
+
+		if (show_demo_window)
+			ImGui::ShowDemoWindow(&show_demo_window);
+
+		{
+			static float f = 0.0f;
+			static int counter = 0;
+
+			ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+			if (ImPlot::BeginPlot("MyPlot"))
+			{
+				ImPlot::PlotBars("My Bar Plot", bar_data, 2);
+				ImPlot::PlotLine("My Line Plot", x_data, y_data, 11);
+				ImPlot::EndPlot();
+			}
+
+			ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+			ImGui::Checkbox("Another Window", &show_another_window);
+
+			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+			if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+				counter++;
+			ImGui::SameLine();
+			ImGui::Text("counter = %d", counter);
+
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			ImGui::End();
+		}
+
+		// 3. Show another simple window.
+		if (show_another_window)
+		{
+			ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+			ImGui::Text("Hello from another window!");
+			if (ImGui::Button("Close Me"))
+				show_another_window = false;
+			ImGui::End();
+		}
+
+		ImGui::Render();
+		SDL_RenderClear(initVars->renderer); //remove anything already rendered
+		WFCComponent->RenderWFC(initVars->renderer); //render WFC 
+		ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData(), initVars->renderer);
+		SDL_RenderPresent(initVars->renderer); //moving from back buffer to screen
 	}
 
-	SDL_DestroyRenderer(sdlVars->renderer);
-	SDL_DestroyWindow(sdlVars->window);
 	delete WFCComponent;
+
+	// Cleanup
+	ImGui_ImplSDLRenderer2_Shutdown();
+	ImGui_ImplSDL2_Shutdown();
+	ImGui::DestroyContext();
+	ImPlot::DestroyContext();
+	SDL_DestroyRenderer(initVars->renderer);
+	SDL_DestroyWindow(initVars->window);
 	IMG_Quit();
 	SDL_Quit();
 
